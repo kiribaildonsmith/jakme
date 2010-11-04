f9407247cf1c641520d8ff0a23adab6634e16a15
JAH <joseph.hallett@googlemail.com>
2010-11-04 14:19:12 +0000

    initial version of sendText

--- src/backend.py
+++ src/backend.py
@@ -5,14 +5,25 @@ class Backend:
   """Implements the backend of the JaKme text editor-a system of
 piping text around.
   """
-  environment = {}
+  environment = {"FILENAME":None}
 
-  def __init__(self, filename):
-    """Create a new Backend object with the path set initially to 'filename'
+  def __init__(self, env=None):
+    """Create a new backend with an optional environment"""
+    if env != None:
+      for k,v in env.iteritems():
+        self.environment[k] = v
+
+  def sendText(self, command, inputText):
+    """Send a body of text through a command. Return the output and
+error messages of that command.
     """
-    self.environment["FILENAME"] = filename
+    pipe = subprocess.Popen(command, stdin=subprocess.PIPE, 
+                                     stdout=subprocess.PIPE, 
+                                     stderr=subprocess.PIPE,
+                                     env=self.environment)
+
 
 if __name__ == '__main__':
-  backend = Backend("test.sh")
-  if (backend.environment["FILENAME"] != "tst.sh"):
+  backend = Backend({"FILENAME":"test.sh"})
+  if (backend.environment["FILENAME"] != "test.sh"):
     print("Failed test 1")
