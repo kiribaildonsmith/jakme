48f5e231a973248bea58b7029fc5a0c70a6ecf59
KBS <kiri.baildonsmith@gmail.com>
2010-12-09 14:17:27 +0000

    Dynamically creating buttons

--- src/gui.py
+++ src/gui.py
@@ -46,31 +46,59 @@ def refresh_window():
 
 
 
-
 if __name__ == "__main__":
 	"""Will implement a gui using the Jakme backend"""
 	root = Tk()
 
 	label_text = StringVar()
+	backend = Backend()
+
+
+
+	# create the buttons
+	buttons = []
+	globalcommands = backend.get_global_commands()
+	regionalcommands = backend.get_regional_commands()
+	print str(globalcommands)
+
+	for text, path in globalcommands.iteritems():
+		print "Creating button: "+text
+		button = Button(root, text=text, command=(lambda: backend.send_text(path, '')))
+		buttons.append(button)
+
+	for text, path in regionalcommands.iteritems():
+		print "Creating button: "+text
+		button = Button(root, text=text, command=(lambda: backend.send_text(path, '')))
+		buttons.append(button)
+
 
 	filename = Entry(root, width=100)
-	Open = Button(root, text="Open", command = open_file)
-	save = Button(root, text="Save", command = save_file)
-	close = Button(root, text="Close", command = close_editor)
-	copy = Button(root, text="Copy", command = copy_selected)
-	cut = Button(root, text="Cut", command = cut_selected)
-	refresh = Button(root, text="Refresh", command = refresh_window)
+	#Open = Button(root, text="Open", command = open_file)
+	#save = Button(root, text="Save", command = save_file)
+	#close = Button(root, text="Close", command = close_editor)
+	#copy = Button(root, text="Copy", command = copy_selected)
+	#cut = Button(root, text="Cut", command = cut_selected)
+	#refresh = Button(root, text="Refresh", command = refresh_window)
 	editor = Text(root, height=40, width=150)
 	feedback = Label(root, textvariable = label_text, width=100, fg="red")
 
+	
+
+
+
+	
+	i = 1
+	for button in buttons:
+		button.grid(column=i, row=0)
+		i = i+1
 
 	filename.grid(column=0, row=0)
-	Open.grid(column=1, row=0)
-	save.grid(column=2, row=0)
-	close.grid(column=3, row=0)
-	copy.grid(column=4, row=0)
-	cut.grid(column=5, row=0)
-	refresh.grid(column=6, row=0)
+	#Open.grid(column=1, row=0)
+	#save.grid(column=2, row=0)
+	#close.grid(column=3, row=0)
+	#copy.grid(column=4, row=0)
+	#cut.grid(column=5, row=0)
+	#refresh.grid(column=6, row=0)
 	editor.grid(column=0, row=1, columnspan=7)
 	feedback.grid(column=0, row=2)
 
